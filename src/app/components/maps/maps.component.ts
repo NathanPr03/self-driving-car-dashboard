a878ac16570a9c562b0a7707cf7d7324609a81b2
Nathan <nathan.pringle@n-able.com>
2022-11-12 14:38:00 +0000

    Add marker to map

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -7,12 +7,24 @@ import { GoogleMapsModule } from "@angular/google-maps";
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
+  center: google.maps.LatLngLiteral = {lat:55.933150041161134, lng:-3.2139476806949494};
+  zoom = 12;
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  addMarker() {
+    // @ts-ignore
+    this.markerPositions.push({
+      lat:55.933150041161134,
+      lng:-3.2139476806949494
+    });
+  }
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-
+    this.addMarker();
   }
 }
